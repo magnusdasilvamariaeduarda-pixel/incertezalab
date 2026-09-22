5efb9881a13e9234a007de9513653f71a2510441
magnusdasilvamariaeduarda-pixel <magnusdasilvamariaeduarda@gmail.com>
2026-09-22 14:05:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -90,8 +90,6 @@ st.image(
     width=500
 )
 
-st.warning("TESTE DO BOTÃO")
-
 st.link_button(
     "📄 Manual de Uso",
     "https://github.com/magnusdasilvamariaeduarda-pixel/incertezalab/raw/main/Manual_MMG_IncertezaLab.pdf",

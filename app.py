70bbd99b687b5841bebfb05e438655c7763d97ce
magnusdasilvamariaeduarda-pixel <magnusdasilvamariaeduarda@gmail.com>
2026-09-22 12:11:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,55 @@
 import streamlit as st
+import numpy as np
+
+st.set_page_config(
+    page_title="IncertezaLab",
+    page_icon="📊"
+)
 
 st.title("📊 IncertezaLab")
 
 grandeza = st.selectbox(
     "Grandeza",
-    ["Temperatura", "Pressão"]
+    [
+        "Temperatura",
+        "Pressão",
+        "Comprimento",
+        "Massa",
+        "Tensão"
+    ]
 )
 
-st.write("Grandeza selecionada:", grandeza)
+unidade = st.text_input(
+    "Unidade",
+    "°C"
+)
+
+medicoes_texto = st.text_area(
+    "Medições (uma por linha)",
+    placeholder="72.4\n72.5\n72.3"
+)
+
+if st.button("🧮 Calcular"):
+
+    try:
+
+        medicoes = [
+            float(x)
+            for x in medicoes_texto.splitlines()
+            if x.strip()
+        ]
+
+        media = np.mean(medicoes)
+
+        st.success("Cálculo realizado!")
+
+        st.metric(
+            "Valor Médio",
+            f"{media:.4f} {unidade}"
+        )
+
+    except:
+
+        st.error(
+            "Verifique as medições informadas."
+        )

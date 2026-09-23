dfcb7640644932f0a93ca29d68a30e0a53f2c3ef
magnusdasilvamariaeduarda-pixel <magnusdasilvamariaeduarda@gmail.com>
2026-09-23 11:00:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -161,28 +161,18 @@ st.subheader("Fontes de Incerteza Tipo B")
 c1, c2, c3 = st.columns(3)
 
 with c1:
-    u_certificado = st.number_input(
-        "Certificado",
-        min_value=0.0,
-        value=0.25,
-        step=0.01
-    )
-
-with c2:
-    u_resolucao = st.number_input(
-        "Resolução",
-        min_value=0.0,
-        value=0.03,
-        step=0.01
-    )
-
-with c3:
-    u_deriva = st.number_input(
-        "Deriva",
-        min_value=0.0,
-        value=0.00,
-        step=0.01
-    )
+instrumentos = {
+    "Paquímetro": {
+        "certificado": 0.02,
+        "resolucao": 0.05,
+        "deriva": 0.01
+    },
+    "Micrômetro": {
+        "certificado": 0.01,
+        "resolucao": 0.01,
+        "deriva": 0.005
+    }
+}
 
 st.divider()
 
